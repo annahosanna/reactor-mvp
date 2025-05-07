2b34dbab3d4932075ad990ad8a0bc8ccd8de9997
annahosanna <annahosanna@gmail.com>
2025-05-07 02:56:09 -0400

    Add extra files

--- make-test3.sh
+++ make-test3.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+reps=5000
+repsdec=${reps}".0"
+echo 'time1=$(date +%s)' > ./test3.sh
+echo 'curl -K ./test3.cfg 2>./err-log.txt >/dev/null' >> ./test3.sh
+echo 'time2=$(date +%s)' >> ./test3.sh
+echo 'singleconnection=`awk "BEGIN { print ${time2} - ${time1} }"`' >> ./test3.sh
+echo 'rps=`awk "BEGIN { print '${repsdec}'/(${time2} - ${time1}) }"`' >> ./test3.sh
+echo 'spr=`awk "BEGIN { print (${time2} - ${time1})/'${repsdec}' }"`' >> ./test3.sh
+echo 'echo "Time to perform '${reps}' request on a single connection: ${singleconnection}. Requests per second: ${rps}. Seconds per request: ${spr}."' >> ./test3.sh
+
 cat << EOF > ./test3.cfg
 cacert = ./certs.pem
 data = "k1=v1&k2=\"\"&k3=&k4=%00"
--- src/main/java/example/MVP.java
+++ src/main/java/example/MVP.java
@@ -18,7 +18,7 @@ public final class MVP {
   // On client, increasing connection timeout results in fewer errors; however, it also results in more open file descriptors - which you might then run out of. The CPU speed affects eventloop speed which in turn affects errors
   // unless a custom event loop is specified multi connection performance is terrible
   // Some sites end response with \n while others do not
-
+  // I haven't tried tuning the ChannelOptions yet, just put in some recommened values
   public static void main(String[] args) throws Exception {
     LoopResources eventLoopGroup = LoopResources.create(
       "eventLoopGroup",
@@ -67,11 +67,14 @@ public final class MVP {
             return res.sendString(responseContent);
           })
           .get("/fortune", (req, res) -> {
-            res.header("content-type", "text/html");
             res.status(200);
+
             String str = new String(
-              "<!DOCTYPE html><html><head></head><body>Thanks for visiting</body></html>\n"
+              "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"3 3 16 16\"><g transform=\"matrix(1.99997 0 0 1.99997-10.994-2071.68)\" fill=\"#da4453\"><rect y=\"1037.36\" x=\"7\" height=\"8\" width=\"8\" fill=\"#32c671\" rx=\"4\"/><path d=\"m123.86 12.966l-11.08-11.08c-1.52-1.521-3.368-2.281-5.54-2.281-2.173 0-4.02.76-5.541 2.281l-53.45 53.53-23.953-24.04c-1.521-1.521-3.368-2.281-5.54-2.281-2.173 0-4.02.76-5.541 2.281l-11.08 11.08c-1.521 1.521-2.281 3.368-2.281 5.541 0 2.172.76 4.02 2.281 5.54l29.493 29.493 11.08 11.08c1.52 1.521 3.367 2.281 5.54 2.281 2.172 0 4.02-.761 5.54-2.281l11.08-11.08 58.986-58.986c1.52-1.521 2.281-3.368 2.281-5.541.0001-2.172-.761-4.02-2.281-5.54\" fill=\"#fff\" transform=\"matrix(.0436 0 0 .0436 8.177 1039.72)\" stroke=\"none\" stroke-width=\"9.512\"/></g></svg>"
             );
+            // System.out.println(str);
+            // "<!DOCTYPE html><html><head></head><body>Thanks for visiting</body></html>\n"
+            res.header("content-type", "image/svg+xml");
             res.header("content-length", String.valueOf(str.length()));
             Mono<String> responseContent = Mono.just(str);
 
