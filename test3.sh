44cb1bf0fd1a022f70031b686b819267f899cd9d
annahosanna <annahosanna@gmail.com>
2025-05-07 02:59:53 -0400

    Remove test3.sh

--- test3.sh
+++
@@ -1,7 +0,0 @@
-time1=$(date +%s)
-curl -K ./test3.cfg 2>./err-log.txt >/dev/null
-time2=$(date +%s)
-singleconnection=`awk "BEGIN { print ${time2} - ${time1} }"`
-rps=`awk "BEGIN { print 5000.0/(${time2} - ${time1}) }"`
-spr=`awk "BEGIN { print (${time2} - ${time1})/5000.0 }"`
-echo "Time to perform 5000 request on a single connection: ${singleconnection}. Requests per second: ${rps}. Seconds per request: ${spr}."
